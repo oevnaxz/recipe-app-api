f3a40e8616cedbda008d55ee80c8eccddd6a2fed
oevnaxz <55879565+oevnaxz@users.noreply.github.com>
2022-10-26 19:50:15 +0800

    Fixed to pass flake8 linting.

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,15 +7,14 @@ from django.test import SimpleTestCase
 from app import calc
 
 class CalcTest(SimpleTestCase):
+    
     """Test the calc module."""
-
 
     def test_add_numbers(self):
         """Adding numbers together."""
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-
 
     def test_subtract_numbers(self):
         """Subtracting numbers together."""
