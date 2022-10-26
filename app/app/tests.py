2bcca4df080aa01f61051bc408f1e298df03b3d1
oevnaxz <55879565+oevnaxz@users.noreply.github.com>
2022-10-26 20:29:28 +0800

    Fixed to pass flake8 linting.

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,9 +6,9 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
     """Test the calc module."""
-
 
     def test_add_numbers(self):
         """Adding numbers together."""
